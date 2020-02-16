9a732a338da8fad483db07341e146c40e65bf421
Marvin Torisaen <marvin.torisaen@outlook.fr>
2020-02-16 16:01:19 +0100

    Database implementation

---
+++ HomeSecure/Migrations/202002061919598_Initialisation.cs
@@ -0,0 +1,60 @@
+﻿namespace HomeSecure.Migrations
+{
+    using System;
+    using System.Data.Entity.Migrations;
+    
+    public partial class Initialisation : DbMigration
+    {
+        public override void Up()
+        {
+            CreateTable(
+                "dbo.Detectors",
+                c => new
+                    {
+                        Id = c.Long(nullable: false, identity: true),
+                        Name = c.String(),
+                        CreationDate = c.DateTime(nullable: false),
+                        UpdateDate = c.DateTime(nullable: false),
+                        RoomId = c.Long(nullable: false),
+                    })
+                .PrimaryKey(t => t.Id)
+                .ForeignKey("dbo.Rooms", t => t.RoomId, cascadeDelete: true)
+                .Index(t => t.RoomId);
+            
+            CreateTable(
+                "dbo.Moves",
+                c => new
+                    {
+                        Id = c.Long(nullable: false, identity: true),
+                        Date = c.DateTime(nullable: false),
+                        DetectorId = c.Long(nullable: false),
+                    })
+                .PrimaryKey(t => t.Id)
+                .ForeignKey("dbo.Detectors", t => t.DetectorId, cascadeDelete: true)
+                .Index(t => t.DetectorId);
+            
+            CreateTable(
+                "dbo.Rooms",
+                c => new
+                    {
+                        Id = c.Long(nullable: false, identity: true),
+                        Name = c.String(),
+                        CreationDate = c.DateTime(nullable: false),
+                        UpdateDate = c.DateTime(nullable: false),
+                    })
+                .PrimaryKey(t => t.Id);
+            
+        }
+        
+        public override void Down()
+        {
+            DropForeignKey("dbo.Detectors", "RoomId", "dbo.Rooms");
+            DropForeignKey("dbo.Moves", "DetectorId", "dbo.Detectors");
+            DropIndex("dbo.Moves", new[] { "DetectorId" });
+            DropIndex("dbo.Detectors", new[] { "RoomId" });
+            DropTable("dbo.Rooms");
+            DropTable("dbo.Moves");
+            DropTable("dbo.Detectors");
+        }
+    }
+}
---
+++ HomeSecure/Models/HomeSecureContext.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data.Entity;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace HomeSecure.Models
+{
+    public class HomeSecureContext : DbContext
+    {
+        public DbSet<Detector> Detectors { get; set; }
+        public DbSet<Move> Moves { get; set; }
+        public DbSet<Room> Rooms { get; set; }
+    }
+}
